2a60c859d1c66d6b0c3bb7f2b363e20f292f1cb5
Solomon <solomonghebrelibanos@gmail.com>
2024-04-09 19:41:00 -0700

    Fourth commit state and hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
 import "./App.css";
-
-import CampsitesList from "./features/campsites/CampsitesList.js";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesList></CampsitesList>
+      <CampsitesDirectoryPage />
       <Footer />
     </div>
   );
