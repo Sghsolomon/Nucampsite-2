7c279444ce2c687d427fbb418b686401d700c7ee
Solomon <solomonghebrelibanos@gmail.com>
2024-04-21 16:00:27 -0700

    10th commit comments added to the comment file with pop up modal

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -1,16 +1,92 @@
 import { useState } from "react";
-import { Button, Modal, ModalHeader, ModalBody } from "reactstrap";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  FormGroup,
+  Label,
+} from "reactstrap";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { validateCommentForm } from "../../utils/validateCommentForm";
 
 const CommentForm = ({ campsiteId }) => {
   const [modalOpen, setModalOpen] = useState(false);
 
+  const handleSubmit = (values) => {
+    const comments = {
+      campsiteId: parseInt(campsiteId),
+      rating: values.rating,
+      author: values.author,
+      text: values.commentText,
+    };
+    console.log(comments);
+    setModalOpen(false);
+  };
+
   return (
     <>
-      <Button outline onClick={setModalOpen(true)}>
+      <Button outline onClick={() => setModalOpen(true)}>
         {" "}
         <i className="fa fa-pencil fa-lg" /> Add Comment
       </Button>
-      <Modal isOpen={modalOpen}></Modal>
+      <Modal isOpen={modalOpen}>
+        <ModalHeader toggle={() => setModalOpen(false)}>
+          {" "}
+          Add Comment
+        </ModalHeader>
+        <ModalBody>
+          <Formik
+            initialValues={{
+              rating: "",
+              author: "",
+              commentText: "",
+            }}
+            onSubmit={handleSubmit}
+            validate={validateCommentForm}
+          >
+            <Form>
+              <FormGroup>
+                <Label htmlFor="rating">Rating</Label>
+                <Field name="rating" as="select" className="form-control">
+                  <option>Select...</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </Field>
+                <ErrorMessage name="rating">
+                  {(msg) => <p className="text-danger">{msg}</p>}
+                </ErrorMessage>
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="author">Solomon</Label>
+                <Field
+                  name="author"
+                  placeholder="Your Name"
+                  className="form-control"
+                />
+                <ErrorMessage name="author">
+                  {(msg) => <p className="text-danger">{msg}</p>}
+                </ErrorMessage>
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="commentText">Comment</Label>
+                <Field
+                  name="commentText"
+                  as="textarea"
+                  rows="12"
+                  className="form-control"
+                />
+              </FormGroup>
+              <Button type="submit" color="primary">
+                Submit
+              </Button>
+            </Form>
+          </Formik>
+        </ModalBody>
+      </Modal>
     </>
   );
 };
